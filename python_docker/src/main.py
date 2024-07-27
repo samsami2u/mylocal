545deddab1e51afc0800a566316a0d76052b3bb2
Sahin Makwana(desktop admin accouunt) <makwana.sachinm@gmail.com>
2024-07-27 14:04:21 +0530

    committing all the pending files so that can be accessed from laptop

---
+++ python_docker/src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+import redis
+
+r = redis.Redis(host="redis", port=6379)
+app = FastAPI()
+
+import debugpy
+
+debugpy.listen(("0.0.0.0", 5678))
+print("Waiting for client to attach...")
+#debugpy.wait_for_client()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hits")
+def read_root():
+    r.set("foo", "none")
+    r.incr("hits")
+    return {"Number of hits:": r.get("hits"), "foo": r.get("foo")}
